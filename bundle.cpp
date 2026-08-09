6ed002bdfb7857c94f9c51825c15b3039d1e5424
samseasons <185178474+samseasons@users.noreply.github.com>
2026-08-09 16:00:20 +0000

    Add files via upload

--- bundle.cpp
+++ bundle.cpp
@@ -1,1 +1,318 @@
 // clang++ bundle.cpp -o bundle.out && ./bundle.out a/a.js a/y.js
+
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void parse (string & file, vector<string> & imported, map<string, vector<string>> & modules,
+    map<string, string> & texts) {
+    string text;
+    ifstream f(file);
+    if (f.is_open()) {
+        stringstream buffer;
+        buffer << f.rdbuf();
+        text = buffer.str();
+        f.close();
+    } else {
+        texts[file] = "";
+        return;
+    }
+    size_t i;
+    while ((i = text.find(" \n")) != -1) {
+        text.replace(i, 2, "\n");
+    }
+    while ((i = text.find("\n\n")) != -1) {
+        text.replace(i, 2, "\n");
+    }
+    stringstream s(text);
+    vector<string> lines;
+    for (string line; getline(s, line, '\n');) {
+        lines.push_back(line);
+    }
+    bool remove = false;
+    text = "";
+    for (string line : lines) {
+        if (line.substr(line.find_first_not_of("\t "), 2) == "//") {
+            continue;
+        }
+        if (!remove && line.find("/*") != -1 && line.find("//*") == -1) {
+            if (line.find("*/") != -1) {
+                line = line.substr(0, line.find("/*")) + ' ' + line.substr(line.find("*/") + 2);
+            } else {
+                line = line.substr(0, line.find("/*"));
+                remove = true;
+            }
+        }
+        if (remove) {
+            i = line.find("*/");
+            if (i != -1) {
+                line = line.substr(i + 2);
+                remove = false;
+            } else {
+                continue;
+            }
+        }
+        while (line.back() == '\t' || line.back() == ' ') {
+            line.pop_back();
+        }
+        if (!line.empty()) {
+            text += line + "\n";
+        }
+    }
+    string texta = text;
+
+    auto resolve = [&](string f, string file) -> string {
+        if (f.rfind("./", 0) == 0) {
+            f = f.substr(2);
+        }
+        char i = f[0];
+        if (i != '.' && i != '/') {
+            f = file.substr(0, file.find_last_of('/')) + '/' + f;
+        } else if (f.rfind("../", 0) == 0) {
+            while (f.rfind("../", 0) == 0) {
+                f = f.substr(3);
+                file = file.substr(0, file.find_last_of('/'));
+            }
+            f = file.substr(0, file.find_last_of('/')) + '/' + f;
+        }
+        return f.substr(f.length() - 3) == ".js" ? f : f + ".js";
+    };
+
+    map<string, vector<string>> files;
+    files[file] = vector<string>();
+    vector<string> order;
+    i = text.find("import ");
+    while (i != -1) {
+        char t = text[i - 1];
+        if (i != 0 && t != '\t' && t != '\n' && t != ' ') {
+            text = text.substr(i + 6);
+            i = text.find("import ");
+            continue;
+        }
+        i += 6;
+        while (text[i] == ' ') {
+            i++;
+        }
+        text = text.substr(i);
+        i = text.find("from");
+        size_t j = text.find('"');
+        size_t k = text.find("'");
+        vector<string> names;
+        if (i != -1 && (i < j || j == -1) && (i < k || k == -1)) {
+            while (i < text.length()) {
+                char j = text[i - 1];
+                char k = text[i + 4];
+                if ((j == ' ' || j == '}') && (k == ' ' || k == '"' || k == '\'')) {
+                    break;
+                }
+                i += 4;
+                i += text.substr(i).find("from");
+            }
+            string t = text.substr(0, i);
+            j = 0;
+            k = t.find_first_of(" ,{}");
+            while (k != -1) {
+                if (k > j) {
+                    names.push_back(t.substr(j, k - j));
+                }
+                j = k + 1;
+                k = t.find_first_of(" ,{}", j);
+            }
+            if (j < t.length()) {
+                names.push_back(t.substr(j));
+            }
+            i += 5;
+            while (text[i] == ' ') {
+                i++;
+            }
+        } else {
+            i = 0;
+        }
+        string f = string(1, text[i]);
+        if (f == "\"" || f == "'") {
+            text = text.substr(i + 1);
+            i = text.find(f);
+            string f = resolve(text.substr(0, i), file);
+            if (files.find(f) == files.end()) {
+                files[f] = vector<string>();
+                order.push_back(f);
+            }
+            files[f].insert(files[f].end(), names.begin(), names.end());
+        }
+        i = text.find("import ");
+    }
+    modules[file] = order;
+    for (string & i : order) {
+        if (find(imported.begin(), imported.end(), i) == imported.end()) {
+            if (modules.find(i) == modules.end()) {
+                return;
+            } else {
+                vector<string> & mods = modules[i];
+                if (find(mods.begin(), mods.end(), file) == mods.end()) {
+                    return;
+                }
+            }
+        }
+    }
+    vector<string> declares = {"async", "class", "const", "default", "function", "let", "var"};
+    vector<char> defines = {'\n', ' ', '(', ',', '.', '['};
+    i = text.find("export ");
+    while (i != -1) {
+        text = text.substr(i + 7);
+        for (string & name : declares) {
+            i = text.find(name);
+            if (i != -1 && i < 3) {
+                text = text.substr(i + name.length());
+            }
+        }
+        string names = "";
+        i = text.find('\n');
+        if (i != -1) {
+            names = text.substr(0, i);
+        }
+        i = 0;
+        while (i < names.length() && names[i] == ' ') {
+            i++;
+        }
+        vector<string> split;
+        if (i < names.length() && names[i] == '{') {
+            names = names.substr(i + 1);
+            stringstream s(names.substr(0, names.find('}')));
+            string name;
+            while (getline(s, name, ',')) {
+                split.push_back(name);
+            }
+        } else {
+            i = names.find('(');
+            size_t j = names.find('=');
+            if (j == -1 || (i < j && i != -1)) {
+                split.push_back(names);
+            } else {
+                while (j != -1) {
+                    split.push_back(names.substr(0, j));
+                    names = names.substr(j);
+                    j = names.find(',');
+                    if (j == -1) {
+                        break;
+                    }
+                    names = names.substr(j);
+                    j = names.find('=');
+                }
+            }
+        }
+        for (string & name : split) {
+            while (!name.empty() && find(defines.begin(), defines.end(), name[0]) != defines.end()) {
+                name = name.substr(1);
+            }
+            for (char & i : defines) {
+                size_t j = name.find(i);
+                if (j != -1) {
+                    name = name.substr(0, j);
+                }
+            }
+            files[file].push_back(name);
+        }
+        i = text.find("export ");
+    }
+    string base64 = "$0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+
+    auto replace = [&](string & text, string & past, string next) -> string {
+        size_t a = 0;
+        size_t i = past.length();
+        size_t j = next.length();
+        while ((a = text.find(past, a)) != -1) {
+            if (text.length() < a + i + 1) {
+                return text;
+            }
+            if (base64.find(text[a + i]) != -1 || (base64 + "\"'.").find(text[a - 1]) != -1) {
+                a += i;
+                continue;
+            }
+            text = text.substr(0, a) + next + text.substr(a + i);
+            a += j;
+        }
+        return text;
+    };
+
+    text = texta;
+    for (auto & pair : files) {
+        string f = pair.first;
+        string path = f.substr(0, f.length() - 3);
+        for (char & i : path) {
+            if (base64.find(i) == -1) {
+                i = '_';
+            }
+        }
+        for (string & name : pair.second) {
+            text = replace(text, name, name + '_' + path);
+        }
+    }
+    lines = {};
+    stringstream t(text);
+    for (string line; getline(t, line, '\n');) {
+        lines.push_back(line);
+    }
+    text = "";
+    for (string & line : lines) {
+        string a = line;
+        string b = a.substr(0, 1);
+        while (b == "\t" || b == " ") {
+            a = a.substr(1);
+            b = a.substr(0, 1);
+        }
+        if (a.rfind("export default ", 0) == 0) {
+            line = a.substr(15);
+        } else if (a.rfind("export ", 0) == 0) {
+            a = line = a.substr(7);
+            a.erase(a.begin(), find_if_not(a.begin(), a.end(), ::isspace));
+            if (a[0] == '{') {
+                continue;
+            }
+        }
+        if (!line.empty() && a.rfind("import ", 0) != 0) {
+            text += line + "\n";
+        }
+    }
+    texts[file] = text;
+}
+
+void build (string file, string output) {
+    vector<string> imported;
+    vector<string> imports = {file};
+    map<string, vector<string>> modules;
+    map<string, string> texts;
+    while (!imports.empty()) {
+        file = imports[0];
+        if (find(imported.begin(), imported.end(), file) != imported.end()) {
+            imports.erase(remove(imports.begin(), imports.end(), file), imports.end());
+        } else {
+            parse(file, imported, modules, texts);
+            if (modules.find(file) != modules.end()) {
+                vector<string> & mods = modules[file];
+                imports.insert(imports.begin(), mods.begin(), mods.end());
+            }
+            if (texts.find(file) != texts.end()) {
+                imported.push_back(file);
+            }
+        }
+    }
+    string text = "";
+    for (string & file : imported) {
+        text += texts[file];
+    }
+    ofstream f(output);
+    if (f.is_open()) {
+        f << text;
+        f.close();
+    }
+}
+
+int main (int argc, char * argv[]) {
+    build(argc > 1 ? argv[1] : "a/a.js", argc > 2 ? argv[2] : "a/y.js");
+    return 0;
+}
